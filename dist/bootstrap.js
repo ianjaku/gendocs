ce2003eb50d3fa0fec8594884047ca66d262e155
Ian Jakubek <ian@invacto.com>
2020-04-13 16:19:18 +0200

    changed todo

--- dist/bootstrap.js
+++ dist/bootstrap.js
@@ -31,8 +31,19 @@ exports.default = {
   // TODO: init should download the existing pages
 
   TODO: Custom URL
+    - 1. user provides URL
+    - 2. test if CNAME has been set
+      no -> provide instructions to set CNAME
+      yes ->
+        - generate certificate
+        - generate nginx server block file
+        - restart nginx
 
   TODO: show error when insert fails (when meilisearch is down for example)
 
-  TODO: limit docs that a user can create (add to invitation) and/or amount of pages in total?
+
+
+  Future:
+    - Custom css
+    - Limit docs that a user can create (add to invitation) and/or amount of pages in total?
 */
